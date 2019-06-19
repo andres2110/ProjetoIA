539bb80701227ace37853e3a7e846f139f0de939
Kevin Merizalde Oñate <kevinmerizalde19@outlook.com>
2019-06-19 21:55:10 +0100

    mutation 2
    swap mutation

--- ProjCatch_TODO/src/ga/geneticOperators/Mutation2.java
+++ ProjCatch_TODO/src/ga/geneticOperators/Mutation2.java
@@ -12,22 +12,23 @@ public class Mutation2<I extends IntVectorIndividual, P extends Problem<I>> exte
 
     @Override
     public void mutate(I ind) {
-        for(int i= 0; i< ind.getNumGenes();i++){
-
-
-            if(GeneticAlgorithm.random.nextDouble() < probability ) {
-                int aux =ind.getGene(i);
-                int cut;
-                do {
-                    cut = GeneticAlgorithm.random.nextInt(ind.getNumGenes());
-                }while (cut != i);
-                ind.setGene(i,ind.getGene(cut));
-                ind.setGene(cut,aux);
-            }
+       // int[] array = parent.clone();
+        int numGenes = ind.getNumGenes();
+        //get 2 random integers between 0 and size of array
+        int r1 = GeneticAlgorithm.random.nextInt(numGenes);
+        int r2 = GeneticAlgorithm.random.nextInt(numGenes);
+        //to make sure the 2 numbers are different
+        while(r1 == r2){
+            r2 = GeneticAlgorithm.random.nextInt(numGenes);
         }
+
+        //swap array elements at those indices
+        int temp = ind.getGene(r1);
+        ind.setGene(r1,ind.getGene(r2));
+        ind.setGene(r2,temp);
     }
 
     @Override
     public String toString() {
-        return " exchange mutation (" + probability + ")";}
+        return " swap mutation (" + probability + ")";}
 }
--- ProjCatch_TODO/src/gui/PanelParameters.java
+++ ProjCatch_TODO/src/gui/PanelParameters.java
@@ -33,7 +33,7 @@ public class PanelParameters extends PanelAtributesValue {
     JComboBox comboBoxRecombinationMethods = new JComboBox(recombinationMethods);
     JTextField textFieldProbRecombination = new JTextField(PROB_RECOMBINATION, TEXT_FIELD_LENGHT);
 
-    String[] mutationMethods = {"Insert", "exchange mutation", "Inversion Mutation (SIM)"};
+    String[] mutationMethods = {"Insert", "Swap mutation", "Inversion Mutation (SIM)"};
     JComboBox comboBoxMutationMethods = new JComboBox(mutationMethods);
     JTextField textFieldProbMutation = new JTextField(PROB_MUTATION, TEXT_FIELD_LENGHT);
 
